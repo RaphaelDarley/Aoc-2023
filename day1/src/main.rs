c58d5e1ced68431fb58eb607687fa0add3169225
Raphael Darley <raphael@raphaeldarley.com>
2023-12-03 02:02:20 +0000

    d1-p1 solved

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,14 +3,74 @@ fn main() {
 
     let sum: usize = input
         .lines()
-        .map(|l| l.chars().filter(|c| c.is_numeric()))
-        .map(|s| {
-            let numstr: String = s.collect();
-            let first = numstr.chars().next().unwrap();
-            let last = numstr.chars().last().unwrap();
-            let num: usize = [first, last].iter().collect::<String>().parse().unwrap();
-            num
+        .map(NumsParse::nums_parse)
+        .map(Iterator::collect::<Vec<usize>>)
+        .map(|nums| {
+            let first = nums.first().unwrap();
+            let last = nums.last().unwrap();
+            first * 10 + last
         })
         .sum();
     println!("sum: {sum}");
 }
+
+const DIGITS: [&str; 10] = [
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+struct NumsParser {
+    inner: String,
+}
+
+impl Iterator for NumsParser {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        for (i, d) in DIGITS.iter().enumerate() {
+            if let Some(_) = self.inner.strip_prefix(d) {
+                self.inner = self.inner.chars().skip(1).collect();
+                return Some(i);
+            }
+        }
+
+        for (i, d) in ('0'..='9').into_iter().enumerate() {
+            if let Some(_) = self.inner.strip_prefix(d) {
+                self.inner = self.inner.chars().skip(1).collect();
+                return Some(i);
+            }
+        }
+        self.inner = self.inner.chars().skip(1).collect();
+        if self.inner.is_empty() {
+            None
+        } else {
+            self.next()
+        }
+    }
+}
+
+trait NumsParse {
+    fn nums_parse(self) -> NumsParser
+    where
+        Self: Sized;
+}
+
+impl<T: ToString> NumsParse for T {
+    fn nums_parse(self) -> NumsParser
+    where
+        Self: Sized,
+    {
+        NumsParser {
+            inner: self.to_string(),
+        }
+    }
+}
+
+// impl<T: Iterator<Item = char>> NumsParse for T {
+//     fn nums_parse(self) -> NumsParser
+//     where
+//         Self: Sized,
+//     {
+//         let string = self.collect();
+//         println!("making nums parser from:{}", string);
+//         NumsParser { inner: string }
+//     }
+// }
